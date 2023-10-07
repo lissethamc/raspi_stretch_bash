e8d16780b27c66e05ab784e6fb52887f288d132e
Lisseth Martínez <33168405+lissethamc@users.noreply.github.com>
2023-10-07 16:11:03 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#Rename with the name of the file containing the new mirrors, make sure you're in the same network as your raspberrys and you replace the "ipn" to the corresponding ips, without removing the "", and replace the "user" for the actual name of the user in your devices, in our case, it's the same for all our 7 devices so it can work
+#Rename with the name of the file containing the new mirrors, 
+#make sure you're in the same network as your raspberrys and you replace the "ipn" 
+#to the corresponding ips, without removing the "", and replace the "user" for the 
+#actual name of the user in your devices, in our case, it's the same for all our 7 devices so it can work
 NEW_SOURCES_FILE="newsources.list"
 
 TURTLEBOT_HOSTS=("ip1" "ip2" "ip3" "ip4" "ip5" "ip6" "ip7")
